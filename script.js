3f2e3df9ccd01ae321316d5b36e9ac231f7ef70d
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-04-19 21:13:00 +0530

    change the apiKey

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const apiKey = "78ae816692243d278ad32abc37245419";
-const apiUrl = "https://home.openweathermap.org/api_keys";
+// const apiKey = "78ae816692243d278ad32abc37245419";
+// const apiUrl = "https://home.openweathermap.org/api_keys";
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
 const weatherIcon = document.querySelector(".weather-icon")
