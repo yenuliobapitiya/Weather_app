e83ab88252e11583c563fff9d126592845879d4f
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-04-19 21:04:49 +0530

    update the script.js

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 const apiKey = "78ae816692243d278ad32abc37245419";
 const apiUrl = "https://home.openweathermap.org/api_keys";
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl+ city + '&appid=${apiKey}');
+
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.winds.speed + "km/h";
+
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+    }
+}
