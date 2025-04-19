c6709b5859cd3d8896d01818cf26562d65c084be
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-04-19 22:31:58 +0530

    fix the issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const apiKey = "78ae816692243d278ad32abc37245419";
+// const apiKey = "78ae816692243d278ad32abc37245419";
 // const apiUrl = "https://home.openweathermap.org/api_keys";
-// const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input")
@@ -8,7 +8,7 @@ const searchBtn = document.querySelector(".search button")
 const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl+ city + '&appid=${apiKey}');
+    const response = await fetch(apiUrl+ city + `&appid=${apiKey}`);
 
     if(response.status == 404) {
         document.querySelector(".error").style.display = "block";
