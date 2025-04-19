d42669ff3d22c056bf7175833543e62f5fe7edfb
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-04-19 21:09:33 +0530

    update the script.js

--- script.js
+++ script.js
@@ -33,5 +33,14 @@ async function checkWeather(city) {
         else if(data.weather[0].main == "Mist") {
             weatherIcon.src = "images/mist.png";
         }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
 }
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+checkWeather();
